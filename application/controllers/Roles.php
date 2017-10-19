57f7cd6b6ea76b4f32214a29bc8ac22a011610dd
Shawn <sdf1342@gmail.com>
2017-10-19 13:17:43 -0700

    on progress of job 7to save to pull and sync to proceed job6

--- application/controllers/Roles.php
+++ application/controllers/Roles.php
@@ -5,3 +5,11 @@
  * Date: 2017-10-19
  * Time: 1:00 PM
  */
+class Roles extends Application {
+
+    public function actor($role = ROLE_GUEST) {
+        $this->session->set_userdata('userrole',$role);
+        redirect($_SERVER['HTTP_REFERER']); // back where we came from
+    }
+
+}
