2547ab06a64a34a8c98dc02aa72a4f3baead115c
Brayden Traas <btraas@gmail.com>
2017-10-19 13:05:30 -0700

    4.1 - item list, by status

---
+++ application/controllers/Mtce.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mtce extends Application
+{
+
+    public function index()
+    {
+        $this->data['pagetitle'] = 'BrayDong TODO List Maintenance';
+        $tasks = $this->tasks->all(); // get all the tasks
+
+        // substitute the status name
+        foreach ($tasks as $task)
+            if (!empty($task->status))
+                $task->status = $this->app->status($task->status);
+
+        // convert the array of task objects into an array of associative objects
+        foreach ($tasks as $task)
+            $converted[] = (array) $task;
+
+        // and then pass them on
+        $this->data['display_tasks'] = $converted;
+        $this->data['pagebody'] = 'itemlist';
+        $this->render();
+    }
+}
