1eccaa1e0ed57bcb2d163c22e0af6b3e6e5f7d4a
Brayden Traas <btraas@gmail.com>
2017-11-03 01:39:57 -0700

    Closes #43, closes #48 - TaskListTest done

--- tests/TaskListTest.php
+++ tests/TaskListTest.php
@@ -1,7 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kimdongwon
- * Date: 2017-11-02
- * Time: 2:19 PM
- **/
+
+if (! class_exists('PHPUnit_Framework_TestCase'))
+{
+    class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+}
+
+
+class CITest2 extends PHPUnit_Framework_TestCase {
+    private $CI;
+
+    public function setUp() {
+        // Load CI instance normally
+        $this->CI = &get_instance();
+    }
+
+    public function testUncompleted() {
+
+        $data = (new Tasks())->all();
+
+        $total = count($data);
+
+        $undone = 0;
+
+        foreach ($data as $task)
+        {
+            if ($task->status != 2)
+                $undone++;
+        }
+
+
+        $this->assertGreaterThan($total-$undone, $undone);
+
+    }
+
+
+}
